42d5d197e9cd86ad4f15746d8785e78534ecee51
zak91831 <155806900+zak91831@users.noreply.github.com>
2025-04-16 17:48:45 -0400

    Add CacheXSSDetector/tests/conftest.py 1744840125060

---
+++ CacheXSSDetector/tests/conftest.py
@@ -0,0 +1,9 @@
+"""
+Pytest configuration file.
+"""
+
+import os
+import sys
+
+# Add the parent directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
