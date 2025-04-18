c361aca87c4bcf81c8a01d5ac28df1d48e36135a
zak91831 <155806900+zak91831@users.noreply.github.com>
2025-04-18 03:08:09 -0400

    Add CacheXSSDetector/utils/__init__.py 1744960088484

---
+++ CacheXSSDetector/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utilities for CacheXSSDetector
+
+This package contains utility modules that support the functionality of CacheXSSDetector.
+"""
+
+from .rate_limiter import RateLimiter
+
+__all__ = ['RateLimiter']
